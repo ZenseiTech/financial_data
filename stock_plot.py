42f89d2c8e28d73efc252d5bf2aa6f147203d7de
ZenseiTech <barizonte@gmail.com>
2024-06-30 17:37:23 -0400

    Added duckdb

--- stock_plot.py
+++ stock_plot.py
@@ -6,9 +6,13 @@ import numpy as np
 from stock_load import load_stock
 
 
-def make_plot(stock_df, field, stock_label):
+def make_plot(stock_df, field, stock_label, avg=0):
     """Create the plot."""
     column = getattr(stock_df, field)
+
+    if avg:
+        column = column.rolling(avg, min_periods=1).mean()
+
     column = np.array(column, dtype="float")
     plt.plot(stock_df["Date"], column, label=stock_label)
     plt.legend()
@@ -45,6 +49,39 @@ def do_highlow_plot(stock_df, stock_name):
     plt.show()
 
 
+def do_volume_plot(stock_df, stock_name):
+    """Plot the daily volume of a stock."""
+    make_plot(stock_df, "Volume", "volume")
+    plt.title("Volume for " + stock_name)
+    plt.show()
+
+
+def do_split_plot(stock_df, stock_name):
+    """Do plot function, with subplotting."""
+    plt.subplot(2, 1, 1)  # first row
+    make_plot(stock_df, "Close", "price")
+    plt.title(stock_name + " Price/Volume")
+
+    plt.subplot(2, 1, 2)  # second row
+    make_plot(stock_df, "Volume", "volume")
+
+    plt.show()
+
+
+def do_movingavg_plot(stock_df, stock_name):
+    """Plot price along wiht 180-day moving average line.
+
+    Some analyst believe that when the current price breaks, above it
+    moving-average line, it's poised for a strong gain; and conversely
+    when the current price fall below the moving-average line, that's
+    often the beginning of a big fall
+    """
+    make_plot(stock_df, "Close", "Closing price")
+    make_plot(stock_df, "Close", "180 day average", 180)
+    plt.title(stock_name + " Stock price")
+    plt.show()
+
+
 if __name__ == "__main__":
 
     period = "10y"
@@ -59,5 +96,14 @@ if __name__ == "__main__":
 
     # do_duo_plot(stock1_df, stock2_df, stock_name1, stock_name2)
 
-    do_highlow_plot(stock1_df, stock_name1)
-    do_highlow_plot(stock2_df, stock_name2)
+    stock1_df = stock1_df[-60:].reset_index()
+    # stock2_df = stock2_df[-60:].reset_index()
+
+    # do_highlow_plot(stock1_df, stock_name1)
+    # do_highlow_plot(stock2_df, stock_name2)
+
+    # do_volume_plot(stock1_df, stock_name1)
+
+    # do_split_plot(stock1_df, stock_name1)
+
+    do_movingavg_plot(stock1_df, stock_name1)
